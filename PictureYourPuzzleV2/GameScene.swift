0cd66dcfd952484a8e072e4f310caf86b49ff8d6
Rprandeski12 <rprandeski12@haverfordsd.net>
2019-12-04 12:12:41 -0500

    func for tiles

--- PictureYourPuzzleV2/GameScene.swift
+++ PictureYourPuzzleV2/GameScene.swift
@@ -9,81 +9,58 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    //yo
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+class GameScene: SKScene
+{
+var backgorund = SKSpriteNode()
+var puzzleBackground = SKSpriteNode()
+var tile = SKSpriteNode()
     
-    override func didMove(to view: SKView) {
+    override func didMove(to view: SKView)
+    {
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        let background = SKSpriteNode(imageNamed: "background")
+        backgorund.physicsBody?.affectedByGravity = false
+        background.physicsBody?.allowsRotation = false
+        background.physicsBody?.friction = 0
+        background.physicsBody?.isDynamic = false
+        background.size = CGSize(width: self.frame.width, height: self.frame.height)
+        background.zPosition = 1
+        addChild(background)
+//
+        puzzleBackground.size = CGSize(width: 600, height: 600)
+        puzzleBackground.position = CGPoint(x: -300, y: -300)
+        puzzleBackground.anchorPoint = CGPoint(x: 0, y: 0)
+        puzzleBackground.color = UIColor.gray
+        puzzleBackground.alpha = CGFloat(0.5)
+        puzzleBackground.zPosition = 2
+        addChild(puzzleBackground)
+//
+        addTile(name: "1", point: CGPoint(x: -300, y: 150))
+        addTile(name: "2", point: CGPoint(x: -150, y: 150))
+        addTile(name: "3", point: CGPoint(x:    0, y: 150))
+        addTile(name: "4", point: CGPoint(x: 150, y: 150))
+        addTile(name: "5", point: CGPoint(x: -300, y: 0))
+        addTile(name: "6", point: CGPoint(x: -150, y: 0))
+        addTile(name: "7", point: CGPoint(x: 0, y: 0))
+        addTile(name: "8", point: CGPoint(x: 150, y: 0))
+        addTile(name: "9", point: CGPoint(x: -300, y: -150))
+        addTile(name: "10", point: CGPoint(x: -150, y: -150))
+        addTile(name: "11", point: CGPoint(x: 0, y: -150))
+        addTile(name: "12", point: CGPoint(x: 150, y: -150))
+        addTile(name: "13", point: CGPoint(x: -300, y: -300))
+        addTile(name: "14", point: CGPoint(x: -150, y: -300))
+        addTile(name: "15", point: CGPoint(x:  0, y: -300))
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+      
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func addTile(name: String, point: CGPoint)
+    {
+        let tile = SKSpriteNode(imageNamed: name)
+        tile.size = CGSize(width: 150, height: 150)
+        tile.position = point
+        tile.anchorPoint = CGPoint(x: 0, y: 0)
+        tile.zPosition = 3
+        addChild(tile)
     }
 }
